bef3d3618760cfc8dc6e7fdfd7525e7b9e53be9a
unknown <antonioarjpi@gmail.com>
2022-04-05 19:34:38 -0300

    Template front

---
+++ backend/src/main/java/com/devsimple/AdoteUmPet/AdoteUmPetApplication.java
@@ -0,0 +1,13 @@
+package com.devsimple.AdoteUmPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteUmPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteUmPetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsimple/AdoteUmPet/AdoteUmPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsimple.AdoteUmPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteUmPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
